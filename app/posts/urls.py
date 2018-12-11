6b31598d8150864f66b916032fcdfd4464f437f6
mingyu <rlaalsrbgk@gmail.com>
2018-12-11 18:41:00 +0900

    post urls에 post_create 추가

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import test_view
+from .views import test_view, post_create
+
+app_name = 'post'
 
 urlpatterns = [
     path('test/', test_view),
+    path('create/', post_create, name='post-create')
 ]
