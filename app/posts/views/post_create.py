7403c9a8093092de6d8d0da297ae126f4a12aa7d
mingyu <rlaalsrbgk@gmail.com>
2018-12-12 21:15:37 +0900

    post view serializer에 따른 generics view로 수정

--- app/posts/views/post_create.py
+++ app/posts/views/post_create.py
@@ -1,20 +1,29 @@
 from django.shortcuts import redirect, render
 
+from posts.serializers.post_create import PostCreateSerializer
+from ..models import Post
 from ..forms import PostForm
 
+from rest_framework import generics
+#
+#
+# def post_create(request):
+#     if request.method == 'POST':
+#         form = PostForm(request.POST)
+#
+#         if form.is_valid():
+#             post = form.save(author=request.user)
+#             return redirect('index')
+#
+#     else:
+#         form = PostForm()
+#         context = {
+#             'form': form,
+#         }
+#
+#     return render(request, 'post/post_create.html', context)
 
-def post_create(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
 
-        if form.is_valid():
-            post = form.save(author=request.user)
-            return redirect('index')
-
-    else:
-        form = PostForm()
-        context = {
-            'form': form,
-        }
-
-    return render(request, 'post/post_create.html', context)
+class PostCreate(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostCreateSerializer
